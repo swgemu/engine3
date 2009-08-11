77dcec8ec818f522e30c0b09323c73a21de4fb0b
theanswer <theanswer@localhost>
2009-08-11 21:48:03 +0000

    [updated] QuadTreeEntry

--- MMOEngine/src/engine/util/QuadTreeEntry.h
+++ MMOEngine/src/engine/util/QuadTreeEntry.h
@@ -43,6 +43,8 @@ namespace engine {
 		}
 
 		inline void addInRangeObject(QuadTreeEntry* obj, bool doNotifyUpdate = true) {
+			//System::out << "adding in range object:" << obj << "\n";
+
 			if (closeobjects.put(obj) != -1)
 				notifyInsert(obj);
 			else if (doNotifyUpdate)
@@ -54,6 +56,8 @@ namespace engine {
 		}
 
 		inline void removeInRangeObject(QuadTreeEntry* obj) {
+			//System::out << "droping in range object:" << obj << "\n";
+
 			if (closeobjects.drop(obj))
 				notifyDissapear(obj);
 			/*else {
@@ -66,11 +70,19 @@ namespace engine {
 		}
 
 		inline void removeInRangeObject(int index) {
+			//System::out << "droping in range object with index:" << index << "\n";
+
 			closeobjects.remove(index);
 		}
 
 		inline void removeInRangeObjects() {
+			//System::out << "droping  all in range objects\n";
+
 			closeobjects.removeAll();
+		}
+
+		inline bool containsInRangeObject(QuadTreeEntry* obj) {
+			return closeobjects.contains(obj);
 		}
 
 		virtual bool isInRange(QuadTreeEntry* obj, float range) {
