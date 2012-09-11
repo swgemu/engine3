d022ac4a5a1943e6d9f2ddfe65e2ab546e14fe7f
theanswer <theanswer@localhost>
2012-09-11 11:55:01 +0000

    [updated] added more classes to sizeoftest

--- MMOEngine/src/testsuite3/tests/sizeoftest.h
+++ MMOEngine/src/testsuite3/tests/sizeoftest.h
@@ -8,6 +8,8 @@
 #ifndef SIZEOFTEST_H_
 #define SIZEOFTEST_H_
 
+#include "engine/util/Facade.h"
+
 #define PRINT_OBJECT_SIZE(OBJ) \
 			printf("size of " #OBJ " is:%ld\n", sizeof(OBJ));
 
@@ -15,14 +17,41 @@
 			assert(sizeof(OBJ) < MAX_SIZE);
 
 void testObjectSizes() {
+	//core
 	PRINT_OBJECT_SIZE(ManagedObject);
 	PRINT_OBJECT_SIZE(ManagedObjectImplementation);
+	PRINT_OBJECT_SIZE(DistributedObject);
+	PRINT_OBJECT_SIZE(DistributedObjectStub);
+	PRINT_OBJECT_SIZE(Task);
+
+	//util
+	PRINT_OBJECT_SIZE(Observer);
+	PRINT_OBJECT_SIZE(ObserverImplementation);
+	PRINT_OBJECT_SIZE(Facade);
+	PRINT_OBJECT_SIZE(FacadeImplementation);
+
+	//u3d
+	PRINT_OBJECT_SIZE(Coordinate);
+	PRINT_OBJECT_SIZE(Quaternion);
+
+	//thread
 	PRINT_OBJECT_SIZE(Lockable);
 	PRINT_OBJECT_SIZE(ReadWriteLock);
+	PRINT_OBJECT_SIZE(Mutex);
 	PRINT_OBJECT_SIZE(pthread_rwlock_t);
+
+	//base
 	PRINT_OBJECT_SIZE(Object);
 	PRINT_OBJECT_SIZE(WeakReference<Object*>);
-	PRINT_OBJECT_SIZE(Mutex);
+	PRINT_OBJECT_SIZE(Reference<Object*>);
+
+	//util
+	PRINT_OBJECT_SIZE(Vector<Object*>);
+	PRINT_OBJECT_SIZE(ArrayList<Object*>);
+	/*PRINT_OBJECT_SIZE(VectorMap);
+	PRINT_OBJECT_SIZE(HashTable);*/
+	PRINT_OBJECT_SIZE(String);
+
 }
 
 #endif /* SIZEOFTEST_H_ */
