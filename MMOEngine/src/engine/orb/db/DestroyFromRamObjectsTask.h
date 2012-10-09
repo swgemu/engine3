190f7d9ba577341cca2a4c356fb47459dffa8c4c
theanswer <theanswer@localhost>
2012-10-09 23:16:16 +0000

    [added] DestroyFromRamObjectsTask

---
+++ MMOEngine/src/engine/orb/db/DestroyFromRamObjectsTask.h
@@ -0,0 +1,34 @@
+/*
+ * DestroyFromRamObjectsTask.h
+ *
+ *  Created on: 10/10/2012
+ *      Author: victor
+ */
+
+#ifndef DESTROYFROMRAMOBJECTSTASK_H_
+#define DESTROYFROMRAMOBJECTSTASK_H_
+
+class DestroyFromRamObjectsTask : public Task {
+	Vector<DistributedObject*>* objects;
+
+public:
+	DestroyFromRamObjectsTask(Vector<DistributedObject*>* obj) {
+		objects = obj;
+	}
+
+	void run() {
+		Logger::console.info("starting to delete " + String::valueOf(objects->size()) + " objects from ram...");
+
+		for (int i = 0; i < objects->size(); ++i) {
+			objects->get(i)->release();
+		}
+
+		delete objects;
+		objects = NULL;
+
+		Logger::console.info("finished deleting objects from ram");
+	}
+};
+
+
+#endif /* DESTROYFROMRAMOBJECTSTASK_H_ */
