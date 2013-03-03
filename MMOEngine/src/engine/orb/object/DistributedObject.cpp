2a7d96cf86d30cd024d48fb1ee508b2e169a6915
theanswer <theanswer@localhost>
2013-03-03 15:39:32 +0000

    [fixed?] delete form db

--- MMOEngine/src/engine/orb/object/DistributedObject.cpp
+++ MMOEngine/src/engine/orb/object/DistributedObject.cpp
@@ -14,7 +14,7 @@ DistributedObject::DistributedObject() : Object() {
 
 	_updated = true;
 	_markedForDeletion = false;
-	_deletedFromDatabase = true;
+	_deletedFromDatabase = false;
 }
 
 DistributedObject::~DistributedObject() {
