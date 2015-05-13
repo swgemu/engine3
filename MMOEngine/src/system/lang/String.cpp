2f3018ac2ffc32042f39bb93a59ef9d0680d2b72
Victor Popovici <theanswer@swgemu.com>
2015-05-13 20:48:34 +0200

    [fixed] compile error

--- MMOEngine/src/system/lang/String.cpp
+++ MMOEngine/src/system/lang/String.cpp
@@ -337,7 +337,9 @@ uint32 String::hashCode() const {
 
 #ifndef CXX11_COMPILER
 uint32 String::hashCode(const char* string, uint32 startCRC) {
-	for (uint32 CRC = startCRC; *string; ++string) {
+	uint32 CRC = startCRC;
+
+	for (; *string; ++string) {
 		CRC = crctable[((CRC>>24) ^ static_cast<byte>(*string)) & 0xFF] ^ (CRC << 8);
 	}
 
