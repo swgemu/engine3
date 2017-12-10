d2e8aea3c9d836b2e951dd7b67a3e9f0708f5acb
Victor Popovici <victorpopovici@gmail.com>
2017-12-10 17:32:29 +0100

    [fixed] IOException on gnu

--- MMOEngine/src/system/io/IOException.h
+++ MMOEngine/src/system/io/IOException.h
@@ -12,9 +12,17 @@ namespace sys {
 	public:
 		IOException(const String& msg) {
 			char str[256];
+#ifdef PLATFORM_MAC
 			int val = strerror_r(errno, str, 256);
 
 			message = msg + " (" + String::valueOf(errno) + ": " + str  +  ")";
+#else
+			char* val = strerror_r(errno, str, 256);
+
+			message = msg + " (" + String::valueOf(errno) + ": " + val  +  ")";
+#endif
+
+
 		}
 
 	};
