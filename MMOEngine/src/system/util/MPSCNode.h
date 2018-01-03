fafd62660df3329630abdbb9d1cefb2c05b7bc29
TheAnswer <victorpopovici@gmail.com>
2018-01-03 10:50:50 +0100

    [added] simple wait free thread safe MPSC queue

---
+++ MMOEngine/src/system/util/MPSCNode.h
@@ -0,0 +1,29 @@
+//
+// Created by theanswer on 1/2/18.
+//
+
+#ifndef ENGINE3_MPSCNODE_H
+#define ENGINE3_MPSCNODE_H
+
+#include <atomic>
+
+namespace sys {
+	namespace util {
+			template<typename Data>
+			class MPSCNode {
+				public:
+					MPSCNode(const Data& a) {
+						data = a;
+					}
+
+					MPSCNode()  = default;
+					MPSCNode(MPSCNode<Data>&&)  = default;
+
+					Data data;
+
+					std::atomic<MPSCNode<Data>*> next;
+			};
+	}
+}
+
+#endif //ENGINE3_MPSCNODE_H
---
+++ MMOEngine/src/system/util/MPSCQueue.h
@@ -0,0 +1,77 @@
+//
+// Created by theanswer on 1/2/18.
+//
+
+#ifndef ENGINE3_MPSCQUEUE_H
+#define ENGINE3_MPSCQUEUE_H
+
+#include "MPSCNode.h"
+
+/**
+ * Wait free Multiple Producers Single Consumer Queue
+ * http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue
+ */
+namespace sys {
+	namespace util {
+		template<class A>
+		class MPSCQueue {
+		private:
+			std::atomic<MPSCNode<A>*> head;
+			MPSCNode<A>* tail;
+
+		public:
+			MPSCQueue(int capacity = 0/*unused*/) {
+				auto head = new MPSCNode<A>();
+
+				this->head = head;
+				head->next = nullptr;
+
+				tail = head;
+			}
+
+			~MPSCQueue() {
+				delete tail;
+			}
+
+			void push(A data) {
+				push(new MPSCNode<A>(data));
+			}
+
+			void push(MPSCNode<A>* n) {
+				n->next = nullptr;
+
+				MPSCNode<A>* prev = std::atomic_exchange_explicit(&this->head, n, std::memory_order_acq_rel);
+				prev->next.store(n, std::memory_order_release);
+			}
+
+			bool popValue(A& val) {
+				auto node = popNode();
+
+				if (node != nullptr) {
+					val = node->data;
+
+					delete node;
+
+					return true;
+				}
+
+				return false;
+			}
+
+			MPSCNode<A>* popNode() {
+				MPSCNode<A>* tail = this->tail;
+				MPSCNode<A>* next = tail->next.load(std::memory_order_acquire);
+
+				if (next) {
+					this->tail = next;
+					tail->data = next->data;
+					return tail;
+				}
+
+				return nullptr;
+			}
+		};
+	}
+}
+
+#endif //ENGINE3_MPSCQUEUE_H
