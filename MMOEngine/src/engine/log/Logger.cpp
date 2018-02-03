54ff5c4b175811b14a96bc0879ec499a3e0b5be4
TheAnswer <victorpopovici@gmail.com>
2018-02-03 13:36:24 +0100

    [fixed] moved json.hpp to a diff folder so core3 can use it

--- MMOEngine/src/engine/log/Logger.cpp
+++ MMOEngine/src/engine/log/Logger.cpp
@@ -3,8 +3,9 @@ Copyright (C) 2007 <SWGEmu>. All rights reserved.
 Distribution of this file for usage outside of Core3 is prohibited.
  */
 
+#include "engine/util/json.hpp"
+
 #include "Logger.h"
-#include "json/json.hpp"
 
 AtomicReference<FileWriter*> Logger::globalLogFile = nullptr;
 
