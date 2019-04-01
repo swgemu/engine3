7605cdbda30c3a99d9d98fbaa56b86b695685ef0
Victor Popovici <victorpopovici@gmail.com>
2019-04-01 19:47:44 +0200

    [added] Optional class

---
+++ MMOEngine/src/system/util/Optional.h
@@ -0,0 +1,23 @@
+/**
+ * @author      : theanswer (theanswer@Victors-MacBook-Pro.local)
+ * @file        : Optional
+ * @created     : Monday Apr 01, 2019 19:39:52 CEST
+ */
+
+#ifndef OPTIONAL_H
+
+#define OPTIONAL_H
+
+#include "optional.hpp"
+
+namespace sys {
+namespace util {
+	template<typename T>
+	using Optional = std::experimental::optional<T>;
+ }
+}
+
+using namespace sys::util;
+
+#endif /* end of include guard OPTIONAL_H */
+
